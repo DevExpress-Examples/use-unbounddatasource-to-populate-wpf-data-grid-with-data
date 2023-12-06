736c38cc649fa181e9a5561476e5743d2832f57f
DevExpressExampleBot <fakeEmail>
2023-12-06 18:22:08 +0400

    Source auto update [skip ci]

--- VB/UnboundSource/MainWindow.xaml.vb
+++ VB/UnboundSource/MainWindow.xaml.vb
@@ -1,79 +1,73 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Core
 Imports System.Collections.ObjectModel
 
 Namespace UnboundSource
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DXWindow
 
         Private vm As MyViewModel
 
         Public Sub New()
             vm = New MyViewModel()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub UnboundDataSource_ValueNeeded(ByVal sender As Object, ByVal e As DevExpress.Data.UnboundSourceValueNeededEventArgs)
             Dim ind = e.RowIndex
-            If e.PropertyName = "UnboundFirstName" Then
+            If Equals(e.PropertyName, "UnboundFirstName") Then
                 e.Value = vm.ListPerson(ind).FirstName
             End If
-            If e.PropertyName = "UnboundAge" Then
+
+            If Equals(e.PropertyName, "UnboundAge") Then
                 e.Value = vm.ListPerson(ind).Age
             End If
-            If e.PropertyName = "UnboundGroup" Then
+
+            If Equals(e.PropertyName, "UnboundGroup") Then
                 e.Value = vm.ListPerson(ind).Group
             End If
         End Sub
 
         Private Sub UnboundDataSource_ValuePushed(ByVal sender As Object, ByVal e As DevExpress.Data.UnboundSourceValuePushedEventArgs)
             Dim ind = e.RowIndex
-            If e.PropertyName = "UnboundFirstName" Then
+            If Equals(e.PropertyName, "UnboundFirstName") Then
                 vm.ListPerson(ind).FirstName = CStr(e.Value)
             End If
-            If e.PropertyName = "UnboundAge" Then
-                vm.ListPerson(ind).Age = CInt((e.Value))
+
+            If Equals(e.PropertyName, "UnboundAge") Then
+                vm.ListPerson(ind).Age = CInt(e.Value)
             End If
-            If e.PropertyName = "UnboundGroup" Then
+
+            If Equals(e.PropertyName, "UnboundGroup") Then
                 vm.ListPerson(ind).Group = CBool(e.Value)
             End If
         End Sub
 
         Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            unboundDataSource1.SetRowCount(10)
+            Me.unboundDataSource1.SetRowCount(10)
         End Sub
     End Class
 
     Public Class MyViewModel
+
         Public Sub New()
             CreateList()
         End Sub
 
         Private Sub CreateList()
             ListPerson = New ObservableCollection(Of Person)()
-
-            For i As Integer = 0 To 49
-                Dim p As New Person(i)
+            For i As Integer = 0 To 50 - 1
+                Dim p As Person = New Person(i)
                 ListPerson.Add(p)
-            Next i
+            Next
         End Sub
 
-        Public Property ListPerson() As ObservableCollection(Of Person)
+        Public Property ListPerson As ObservableCollection(Of Person)
     End Class
 
     Public Class Person
@@ -83,13 +77,14 @@ Namespace UnboundSource
             LastName = "LastName" & i
             Age = i * 10
             Group = i Mod 2 = 0
-
         End Sub
 
-        Public Property Age() As Integer
-        Public Property FirstName() As String
-        Public Property Group() As Object
-        Public Property LastName() As String
-    End Class
+        Public Property Age As Integer
 
+        Public Property FirstName As String
+
+        Public Property Group As Object
+
+        Public Property LastName As String
+    End Class
 End Namespace
